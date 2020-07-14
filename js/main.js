098eb2854a8a80fabb3ae290ff666c3f8b9b710c
Nandakishore-Menon <nandakishore.menon@iiitb.org>
2020-07-14 23:44:37 +0530

    sliding added

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
+var l_content=`<button id="back" class="ghost">Back</button>`;
+var r_content=`                <div class="tech ">
+                    <h1 class="text-center">Skills</h1>
+                    <i class="devicon-c-plain-wordmark techic"></i>
+                    <i class="devicon-python-plain techic"></i>
+                    <i class="devicon-java-plain techic"></i><br><br>
+                    <i class="devicon-javascript-plain techic"></i>
+                    <i class="devicon-html5-plain techic"></i>
+                    <i class="devicon-css3-plain techic"></i><br><br>
+                    <i class="devicon-mongodb-plain techic"></i>
+                    <i class="devicon-nodejs-plain techic"></i>
+                    <i class="devicon-express-original techic"></i>
+                </div>
+                <button id="about" class="ghost">More about me?</button>`;
+
+
+var right=function(){
+    $(".blue-box").html(r_content);
+    $("#about").click(function(){
+
+    $(".blue-box").animate({right: '+=50%',
+        opacity: "0.9"
+    });
+    left();
+    $(".blue-box").animate({opacity:"1"});
+
+}); 
+};
+
 var left=function(){
-    $(".blue-box").html(`<button id="back" class="ghost">Back</button>`);
+    $(".blue-box").html(l_content);
+
+    $("#back").click(function(){
+
+        $(".blue-box").animate({right: '-=50%',
+            opacity: "0.9"
+        });
+        right();
+        $(".blue-box").animate({opacity:"1"});
+
+    });
 };
 
 
-$("#about").click(function(){
-    // $(".blue-box").html(`<button id="Back" class="ghost">Back</button>`);
-    // $(".blue-box").animate({right:'20%',opacity:'0.6'});
-    $(".blue-box").animate({right: '50%',
-        opacity: "0.5"
-    },left);
-    $(".blue-box").animate({opacity:"1"});
+ 
+right();
 
-}); 
-$("#back").click(function(){
-    // $(".blue-box").html(`<button id="Back" class="ghost">Back</button>`);
-    // $(".blue-box").animate({right:'20%',opacity:'0.6'});
-    $(".blue-box").animate({right: '50%',
-        opacity: "0.5"
-    },left);
-    $(".blue-box").animate({opacity:"1"});
 
-}); 
 
