f8255058f1b17c7078fe4955e5f52f97a133c435
Nandakishore-Menon <nandakishore.menon@iiitb.org>
2020-07-15 09:40:58 +0530

    size corrections

--- js/main.js
+++ js/main.js
@@ -11,20 +11,51 @@ var r_content=`                <div class="tech ">
                     <i class="devicon-nodejs-plain techic"></i>
                     <i class="devicon-express-original techic"></i>
                 </div>
-                <button id="about" class="ghost">More about me?</button>`;
+                <div class="but">
+                <button id="project" class="ghost">My Projects</button>
+                <button id="about" class="ghost">More about me?</button>
+                </div>`;
 
+var projs=`<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>`;
 
 var right=function(){
     $(".blue-box").html(r_content);
     $("#about").click(function(){
 
-    $(".blue-box").animate({right: '+=50%',
+        $(".blue-box").animate({right: '+=50%',
         opacity: "0.99"
-    });
-    left();
-    $(".blue-box").animate({opacity:"1"});
+        },800);
+        left();
+        $(".blue-box").animate({opacity:"1"});
 
-}); 
+    }); 
+    $("#project").click(function(){
+        $(".blue-box").animate({right: '+=50%',
+        opacity: "0.99"
+        },800);
+        project();
+        $(".blue-box").animate({opacity:"1"});
+    });
 };
 
 var left=function(){
@@ -34,14 +65,26 @@ var left=function(){
 
         $(".blue-box").animate({right: '-=50%',
             opacity: "0.99"
-        });
+        },800);
         right();
         $(".blue-box").animate({opacity:"1"});
 
     });
 };
 
+var project=function(){
+    $(".right").html(projs);
+    $(".blue-box").html(l_content);
+    $("#back").click(function(){
 
+        $(".blue-box").animate({right: '-=50%',
+            opacity: "0.99"
+        },800);
+        right();
+        $(".blue-box").animate({opacity:"1"});
+
+    });
+};
  
 right();
 
