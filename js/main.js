6f1a3e7e6b91be67522c142098b0b402dac22e47
Nandakishore-Menon <nandakishore.menon@iiitb.org>
2020-07-15 15:44:08 +0530

    project updated

--- js/main.js
+++ js/main.js
@@ -21,11 +21,11 @@ var projs=`<div id="carouselExampleControls" class="carousel slide" data-ride="c
     <div class="carousel-item">
       
       <div  class="car-img d-block w-100">
-        <img src="img/me.jpeg" alt="First slide">
+        <img src="img/stu.png" alt="First slide">
       </div>
       <div class="d-none carousel-caption">
-        <h1>Sliding puzzle</h1>
-        <p>Solves the sliding puzzle in minimum moves and also allows user to play.Implemented in python.</p>
+        <h1>Student Database Management System</h1>
+        <p>Uses a REST API to take user info and stores in database, which can later be viewed, searched, updated and deleted.</p>
       </div>
     </div>
     <div class="carousel-item  active">
@@ -74,7 +74,7 @@ var exp=`<div id="exp"><h2>Currently seeking intership opportunities.</h2><butto
 var interest=`<div id="interest"><ul>
     <li><h3>I am passionate about problem solving in general, be it a development challenge or competitive programming questions.</h3></li><br>
     <li><h3>I enjoy CTFs and Hackathons</h3></li><br>
-    <li><h3>I am also a tech enthusiast(more like junkie)</h3></li>
+    <li><h3>I look forward to contributing to Open Source projects</h3></li>
 </ul></div>`;
 
 var cont_info=`<div id="cont_info"><h2>Send me your message,here </h2><span><br>OR THROUGH<br><span>
